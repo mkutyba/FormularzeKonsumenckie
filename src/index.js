b2a76b08f2b356226286b5918a80b39571095e22
Mateusz Kutyba <mateusz.kutyba@gmail.com>
2017-03-13 08:40:09 +0100

    Load Material-UI and wrap application in theme

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+ReactDOM.render((
+    <MuiThemeProvider>
+        <App />
+    </MuiThemeProvider>
+), document.getElementById('root'));
